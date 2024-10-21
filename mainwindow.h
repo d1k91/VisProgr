70c4fdcaed8576be463d375aa48f92e111e9af4c
EduardBaz04 <124115646+EduardBaz04@users.noreply.github.com>
2024-10-21 19:52:08 +0700

    Finish version

--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,45 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTabWidget>
+#include <QFile>
 
-namespace Ui {
-class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_openFile_triggered();
+    void on_saveFile_triggered();
+    void clearAndSave();
+    void restoreFromTemp();
+    void on_fontChange_triggered();
+    void on_colorChange_triggered();
+    void on_search_triggered();
+    void on_replace_triggered();
+    void on_createTable_triggered();
+    void tabChanged(int index);
 
 private:
     Ui::MainWindow *ui;
+    QTabWidget *tabWidget;
+    QFile tempFile;
+    bool isTempFileEmpty;
+    bool isContentModified;
+    QString lastFilePath;
+
+    void loadSettings();
+    void saveSettings();
+    void saveToTempFile();
+    void closeEvent(QCloseEvent *event) override;
 };
 
 #endif // MAINWINDOW_H
